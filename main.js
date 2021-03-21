473b8789bd6e784dfd7a7bf28cf3e0d299fa77ab
jisu yeo <jisuyeo@jisus-air.myfiosgateway.com>
2021-03-21 15:47:35 -0400

    h

--- main.js
+++ main.js
@@ -1,34 +1,46 @@
-const imgContainer = document.querySelector('#img-container')
-const startButton = document.querySelector('.startButton')
-const cell = document.querySelectorAll(".cell")
+const container = document.querySelector('.img-container')
+const startButton = document.querySelector('.start-button')
+const gameText = document.querySelector('.game-text')
+// console.log(gameText)
 
-
-function Ramdomize(res) {
-    return Math.floor(Math.random() * Math.floor(res));
-  }
-  console.log(Ramdomize(cell.length))
-
-
-// function getRandomNum(pic) {
-//     let picPosition = array.length -1;
-//     while (picPosition > 0){
-//         const randomImg = Math.floor(Math.random()*(picPosition+1));
-//         [pic[picPosition], pic[randomImg]] = [pic[randomImg], pic[picPosition]]
-//         picPosition--;
-        
-//     }
-//     return picPosition
-    
-// }
-  
-
-
-document.querySelector('#button').addEventListener('click', () => { 
-    // console.log('clicked')
-    document.querySelector('#faceUp').classList.add('hidden')
-    document.querySelector('#button').classList.add('hidden')
+document.querySelector('.start-button').addEventListener('click',()=> {
+  // console.log('work')
+  document.querySelector('#faceUp').classList.add('hidden')
+  document.querySelector('.start-button').classList.add('hidden')
 })
+///// creat new array for containing creatImgCells function and use mixing(shuffle) function to mix the order of the li.
 
 
 
 
+
+let cells=[];
+cells = creatImgCells();
+// console.log(cells)
+
+miximg(cells).forEach(cell => container.appendChild(cell))
+
+///// refrence form youtube (how to make puzzle form js)
+
+function creatImgCells(){
+  const newArry =[];
+  Array(16).fill().forEach((data,i) =>{
+    // console.log(i)
+    const li = document.createElement('li')
+      li.setAttribute('img-index', i)
+      li.classList.add(`list${i}`)
+      // console.log(li)
+     newArry.push(li);
+     })
+  return newArry
+}
+
+function miximg(array){
+  let index = array.length -1;
+  while(index > 0){
+    const randomImg = Math.floor(Math.random()*(index+1));
+    [array[index], array[randomImg]] = [array[randomImg], array[index]]
+    index--;
+  }
+  return array;
+}
